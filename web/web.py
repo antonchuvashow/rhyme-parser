2c65580074f7066e35b944afdb56ca5ec1cc842d
Anton Chuvashov <antonchuvashow@gmail.com>
2023-12-11 16:01:05 +0500

    add POST and GET checking

--- web/web.py
+++ web/web.py
@@ -1,5 +1,6 @@
 import requests
 from flask import Flask, render_template
+from flask import request
 from sqlalchemy import exc
 
 from db_models.db_session import db
@@ -11,28 +12,26 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
-def main_page():
+def show_main_page():
     search = SearchForm()
-    if search.validate_on_submit():
 
+    if request.method == "POST" and search.validate_on_submit():
         api_url = "http://scrapy:2209/crawl"
         rhymes = requests.post(api_url, json={"word": search.name.data}).json()['message']
 
         word = Word.query.filter_by(word=search.name.data.lower()).first()
+
         if word:
             word.frequency += 1
-            try:
-                db.session.commit()
-            except exc.SQLAlchemyError:
-                db.session.rollback()
-
         else:
             word = Word(word=search.name.data.lower(), frequency=1)
             db.session.add(word)
-            try:
-                db.session.commit()
-            except exc.SQLAlchemyError:
-                db.session.rollback()
+
+        try:
+            db.session.commit()
+        except exc.SQLAlchemyError:
+            db.session.rollback()
 
         return render_template("rhymes.html", form=search, rhymes=rhymes)
+
     return render_template("index.html", form=search, words=get_most_popular_words(limit=40, scale=80, alpha=0.5))
